e2f240d8d4b6d94bd3048fba388a586d4c2d95b1
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-07-10 21:20:11 +0200

    Create new models and run it

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Sensor)
+admin.site.register(SensorType)
+admin.site.register(SensorValueType)
+admin.site.register(Place)
+admin.site.register(Monitoring)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,4 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class SensorValueType(models.Model):
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+
+class SensorType(models.Model):
+    title = models.CharField(max_length=20)
+    value_type = models.ForeignKey(SensorValueType, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Place(models.Model):
+    room = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.room}'
+
 
 class Sensor(models.Model):
-    name =
+    title = models.CharField(max_length=20)
+    sensor_model = models.ForeignKey(SensorType, on_delete=models.SET_NULL, null=True)
+    sensor_place = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True)
+    sensor_owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    info = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f'{self.title}: {self.sensor_model}'
+
+
+class Monitoring(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.DO_NOTHING)
+    value = models.FloatField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.id} {self.sensor} {self.value}'
