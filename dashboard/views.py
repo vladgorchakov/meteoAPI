2d833a9147dd7fb16b487b27c577bd42aaab3069
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-07-11 15:07:12 +0200

    Add Place api

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from .serializers import DashboardSerializer, SensorSerializer, SensorTypeSerializer, \
     SensorValueTypeSerializer, PlaceSerializer
-from .models import Monitoring, Sensor, SensorType, SensorValueType, Place
+from .models import Monitoring, Sensor, SensorType, SensorValueType, Place, User
 
 
 class DashboardListApi(generics.ListCreateAPIView):
